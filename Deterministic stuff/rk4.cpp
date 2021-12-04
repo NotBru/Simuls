af96774675a9106f4d3f7da70727fc12d4c56a56
Bru <bruno.ronchi@muttdata.ai>
2021-12-04 17:36:11 -0300

    Wave equation :D

--- Deterministic stuff/rk4.cpp
+++ Deterministic stuff/rk4.cpp
@@ -1,10 +1,14 @@
 // #include <cmath>
 // #include <functional>
+#include <cmath>
 #include <functional>
-#include <iostream>
+#include <exception>
 #include <fstream>
+#include <iostream>
+#include <numeric>
 #include <string>
 #include <valarray>
+#include <vector>
 
 #include <ginac/ginac.h>
 
@@ -21,19 +25,71 @@ void rk4_step(
 	x+=dt/6*(k1+2*k2+2*k3+k4);
 }
 
-class StateSpace
+class Serializer
 {
-	std::vector<GiNaC::realsymbol> coordinates;
-	std::vector<GiNaC::realsymbol> momentums;
+	std::vector<int> shape;
+	int counter=0;
 
 	public:
-		StateSpace(int num_degrees=0)
+
+	Serializer(std::vector<int> shape):
+		shape{shape}
+	{}
+
+	Serializer operator[](int i) const
+	{
+		std::vector<int> reduced_shape(shape.cbegin()+1, shape.cend());
+		int step=std::accumulate(reduced_shape.cbegin(), reduced_shape.cend(), 1, std::multiplies<int>{});
+
+		Serializer ret{reduced_shape};
+		ret.counter = counter + step * i;
+
+		return ret;
+	}
+
+	operator int() const
+	{
+		return counter;
+	}
+
+	int range() const
+	{
+		int step=std::accumulate(shape.cbegin(), shape.cend(), 1, std::multiplies<int>{});
+		return counter+step;
+	}
+};
+
+class StateSpace
+{
+	std::vector<GiNaC::realsymbol> coordinates,
+	                               momentums;
+	std::vector<std::string> cnames,
+		                 mnames;
+
+	void set_names(int num_degrees, std::vector<std::string> cnames, std::vector<std::string> mnames)
+	{
+		this->cnames=cnames;
+		this->mnames=mnames;
+		for(int i=cnames.size(); i<num_degrees; i++)
+			cnames[i]=std::string("q")+std::to_string(i+1);
+		for(int i=mnames.size(); i<num_degrees; i++)
+			mnames[i]=std::string("p")+std::to_string(i+1);
+	}
+
+	void set_coordinates(int num_degrees)
+	{
+		for(int i=0; i<num_degrees; i++)
 		{
-			for(int i=0; i<num_degrees; i++)
-			{
-				coordinates.push_back(GiNaC::realsymbol(std::string("q")+std::to_string(i+1)));
-				momentums.push_back(GiNaC::realsymbol(std::string("p")+std::to_string(i+1)));
-			}
+			coordinates.push_back(GiNaC::realsymbol(cnames[i]));
+			momentums.push_back(GiNaC::realsymbol(mnames[i]));
+		}
+	}
+
+	public:
+		StateSpace(int num_degrees=0, std::vector<std::string> cnames=std::vector<std::string>(), std::vector<std::string> mnames=std::vector<std::string>())
+		{
+			set_names(num_degrees, cnames, mnames);
+			set_coordinates(num_degrees);
 		}
 
 		GiNaC::realsymbol &q(int i)
@@ -46,6 +102,14 @@ class StateSpace
 			return coordinates[i];
 		}
 
+		std::valarray<GiNaC::ex> valarray_q()
+		{
+			std::valarray<GiNaC::ex> coordinates(this->coordinates.size());
+			for(int i=0; i<coordinates.size(); i++)
+				coordinates[i] = this->coordinates[i];
+			return coordinates;
+		}
+
 		GiNaC::realsymbol &p(int i)
 		{
 			return momentums[i];
@@ -54,6 +118,14 @@ class StateSpace
 		GiNaC::realsymbol p(int i) const
 		{
 			return momentums[i];
+		}
+
+		std::valarray<GiNaC::ex> valarray_p()
+		{
+			std::valarray<GiNaC::ex> momentums(this->momentums.size());
+			for(int i=0; i<momentums.size(); i++)
+				momentums[i] = this->momentums[i];
+			return momentums;
 		}
 
 		GiNaC::realsymbol t=GiNaC::realsymbol("t");
@@ -97,8 +169,8 @@ class StateSpace
 			std::string ret;
 			int dim=coordinates.size();
 			for(int i=0; i<dim; i++)
-				ret += std::string(coordinates[i].get_name()) + "," + momentums[i].get_name();
-			return ret + "," + t.get_name();
+				ret += cnames[i] + "," + mnames[i] + ",";
+			return ret + t.get_name();
 		}
 };
 
@@ -251,20 +323,81 @@ std::function<State(const State, double)> hamilton_equations(StateSpace &ss, GiN
  * Quizas meterles movimiento browniano a las moscas también.
  */
 
+double radial_func(double dist)
+{
+	if(dist > 10) return 0;
+	double sigma=1;
+	return exp(-0.5*dist/sigma*sigma);
+}
+
 int main()
 {
-	auto ss = StateSpace(1); // State space of dimension 1
-	auto hamiltonian = .5 * ss.p(0) * ss.p(0) + .5 * ss.q(0) * ss.q(0);
+	int width=20,
+	    height=20;
+	Serializer szr{{height, width}};
+	int range=szr.range();
+	std::vector<std::string> cnames(range), mnames(range);
+	for(int i=0; i<height; i++) for(int j=0; j<width; j++)
+	{
+		cnames[szr[i][j]]=std::string("q_")+std::to_string(i)+"_"+std::to_string(j);
+		mnames[szr[i][j]]=std::string("p_")+std::to_string(i)+"_"+std::to_string(j);
+	}
+	double v{1};
+	StateSpace ss(range, cnames, mnames); // State space of dimension 1
+	std::valarray<GiNaC::ex> q=ss.valarray_q(), p=ss.valarray_p();
+	GiNaC::ex
+		kinetic_energy{
+			(p*p).sum()/(2*v*v)
+		},
+		potential_energy{
+			0
+		};
+
+	for(int i=1; i<10; i++)
+	{
+		for(int j=0; j<10; j++)
+		{
+			GiNaC::ex neighbour_diff;
+			neighbour_diff = q[szr[i-1][j]]-q[szr[i][j]];
+			potential_energy += neighbour_diff * neighbour_diff;
+			neighbour_diff = q[szr[j][i-1]]-q[szr[j][i]];
+			potential_energy += neighbour_diff * neighbour_diff;
+		}
+		GiNaC::ex border_q;
+		border_q = q[szr[i][0]];
+		potential_energy += border_q*border_q;
+		border_q=q[szr[0][i]];
+		potential_energy += border_q*border_q;
+	}
+	potential_energy /= 2;
+
+	GiNaC::ex hamiltonian{kinetic_energy + potential_energy};
 	auto hamilton_eqs = hamilton_equations(ss, hamiltonian);
-	auto s = State(1); // State vector uwu
-	s.q(0)=1;
-	s.p(0)=0;
-	double dt=0.001;
+	State s(ss);
+	for(int i=0; i<szr.range(); i++)
+	{
+		s.q(i)=0;
+		s.p(i)=0;
+	}
+	for(int i=0; i<height; i++) for(int j=0; j<width; j++)
+	{
+		double mid_height=height%2?height/2:height/2-0.5,
+		       mid_width=width%2?width/2.:width/2-0.5,
+		       dist = pow(
+			        (i-mid_height)*(i-mid_height)+
+			        (j-mid_width)*(j-mid_width),
+				0.5
+			      );
+		s.q(szr[i][j]) = radial_func(dist);
+	}
+	s.p(szr[height/2][width/2+1])=4;
+	s.p(szr[height/2][width/2])=-4;
+	double dt=0.05;
 
 	std::ofstream outf;
 	outf.open("test");
 	outf << ss.csv() << "\n";
-	for(int i=0; i<10000; i++)
+	for(int i=0; i<500; i++)
 	{
 		outf << s.csv() << "," << i*dt << "\n";
 		rk4_step(hamilton_eqs, s, i*dt, dt);
